1001c4f8451936352cb5d866808355e455e8b6da
Ognjen-T <ognjentairovski8@gmail.com>
2026-04-10 13:32:18 +0200

    Improved code

--- script.js
+++ script.js
@@ -2,36 +2,36 @@
 const counter = document.querySelector("#counter");
 const TrueColorDiv = document.querySelector("#TrueColor");
 const PlayerColor = document.querySelector("#PlayerColor");
-
 const Sliders = document.querySelectorAll(".slider");
 
-let CharacterArray = ["0","1","2","3","4","5","6","5","6","7",
-                      "8","9","A", "B", "C", "D", "E", "F"];
 let Color = "";
 let R;
 let G;
 let B;
+let i = 500;
 
 
-    let i = 500;
-function Run(){
+
+
+function GenerateColor(){
     TrueColorDiv.style.display ="block"
     Color = "";
-    let R = Math.floor(Math.random() * 256);
-    let G = Math.floor(Math.random() * 256);
-    let B = Math.floor(Math.random() * 256);
+    R = Math.floor(Math.random() * 256);
+    G = Math.floor(Math.random() * 256);
+    B = Math.floor(Math.random() * 256);
+;
     console.log(R+ " " + G + " "+ B);
     TrueColorDiv.style.backgroundColor = `rgb(`+R+`,`+G+`,`+B+`)`;
+    brightness = (R * 299 + G * 587 + B * 114) / 1000
+    if(brightness <= 128){
+        counter.style.color = "white";
+    }else{
+        counter.style.color = "black";
+    }
+}
 
-    const timer = setInterval(() => {
-        counter.textContent = i;
-        i--;       
-    },10);
 
-    setTimeout(() => {
-        clearInterval(timer);
-        counter.textContent = "";
-
+function PlayerGuessColor(){
         const Red = document.querySelector("#Red");
         const Green = document.querySelector("#Green");
         const Blue = document.querySelector("#Blue");
@@ -44,12 +44,30 @@ function Run(){
             Tick.textContent = vreme;
             Red.oninput = () => {
                 Display.style.backgroundColor = `rgb(${Red.value}, ${Green.value}, ${Blue.value})`;
+                brightness = (Red.value * 299 + Green.value * 587 + Blue.value * 114) / 1000
+                if(brightness <= 128){
+                    Tick.style.color = "white";
+                }else{
+                    Tick.style.color = "black";
+                }
             };
             Green.oninput = () => {
                 Display.style.backgroundColor = `rgb(${Red.value}, ${Green.value}, ${Blue.value})`;
+                brightness = (Red.value * 299 + Green.value * 587 + Blue.value * 114) / 1000
+                if(brightness <= 128){
+                    Tick.style.color = "white";
+                }else{
+                    Tick.style.color = "black";
+                }
             };
             Blue.oninput = () => {
                 Display.style.backgroundColor = `rgb(${Red.value}, ${Green.value}, ${Blue.value})`;
+                brightness = (Red.value * 299 + Green.value * 587 + Blue.value * 114) / 1000
+                if(brightness <= 128){
+                    Tick.style.color = "white";
+                }else{
+                    Tick.style.color = "black";
+                }
             };
             vreme--;
         },10)
@@ -66,9 +84,26 @@ function Run(){
             },200);
           
         },10000);
-        
-    },5000);
+}
 
+
+function WatchTimer(){
+    const timer = setInterval(() => {
+            counter.textContent = i;
+            i--;       
+        },10);
+
+        setTimeout(() => {     
+            clearInterval(timer);
+            counter.textContent = "";
+            PlayerGuessColor()
+        },5000);
+}
+
+
+function Run(){
     
-    
+    GenerateColor();
+
+    WatchTimer();
 }
